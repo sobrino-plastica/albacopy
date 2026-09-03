641a2155827de764a765a902bd1f9150760299b0
sobrino-plastica <jmsobrinoc01@educarex.es>
2026-09-03 22:09:57 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ purpose === 'alumnado'
 ? 'Copias para alumnado'
 : 'Uso personal';
 
-const emailSubject = `[COPIAS IES ALBALAT] Prof. ${cleanCode || 'XXX'} - ${copiesCount} copias`;
+const emailSubject = [COPIAS IES ALBALAT] Prof. ${ cleanCode || 'XXX' } - ${copiesCount} copias;
 
 const emailBody = [
 'Solicitud de fotocopias · IES Albalat',
